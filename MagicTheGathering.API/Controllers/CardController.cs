cab6ed1f1fe6af33b4d67fa480f8607a0a253015
Decimal <DecimalCactus@gmail.com>
2023-12-16 18:38:10 -0500

    Add project files.

---
+++ MagicTheGathering.API/Controllers/CardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicTheGathering.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardController : ControllerBase
+    {
+        [HttpGet]
+        public int GetNumberOfCards()
+        {
+            return 5;
+        }
+    }
+}
+
---
+++ MagicTheGathering.API/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MagicTheGathering.API
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args); //lots of magic... 
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            app.MapControllers();
+
+            //app.MapGet("/", () => "Hello World!"); 
+
+            app.Run();
+        }
+    }
+}
